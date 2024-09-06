c4ef3c8801e641f153eda603632ce55d5a0d6e0b
TheMinimalist <themnmlst.dev@protonmail.com>
2024-09-06 23:17:20 +0200

    cpp

--- nvim/.config/nvim/lua/theminimalist/plugins/lsp.lua
+++ nvim/.config/nvim/lua/theminimalist/plugins/lsp.lua
@@ -70,8 +70,9 @@ return {
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
                 { name = 'luasnip' }, -- For luasnip users.
-            }, {
                 { name = 'buffer' },
+                { name = 'path' },
+            }, {
             })
         })
 
--- nvim/.config/nvim/lua/theminimalist/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/theminimalist/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all"
             ensure_installed = {
-                "vimdoc", "lua", "markdown_inline",
+                "vimdoc", "lua", "markdown_inline", "cpp",
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
