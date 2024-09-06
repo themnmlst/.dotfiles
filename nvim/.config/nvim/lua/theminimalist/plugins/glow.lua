2cae40973ac25445d56dec40d9d01a651a0c6246
TheMinimalist <themnmlst.dev@protonmail.com>
2024-09-07 00:43:59 +0200

    Adding plugins to test.

--- nvim/.config/nvim/lua/theminimalist/plugins/glow.lua
+++ nvim/.config/nvim/lua/theminimalist/plugins/glow.lua
@@ -1,5 +1,5 @@
 return {
-    "ellisonleao/glow.nvim",
-    config = true,
-    vim.keymap.set("n", "<leader>G", vim.cmd.Glow, { desc = "Preview Markdown" })
+	"ellisonleao/glow.nvim",
+	config = true,
+	vim.keymap.set("n", "<leader>G", vim.cmd.Glow, { desc = "Preview Markdown" }),
 }
